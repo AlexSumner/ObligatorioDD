3dd685d35b2d86a8cda82dd39335b76360219515
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2023-10-18 11:45:56 -0300

    Persona constructores

---
+++ src/Arbitro.java
@@ -0,0 +1,23 @@
+public class Arbitro extends Persona{
+    private short Nvl_Certificacion;
+
+    public short getNvl_Certificacion() {
+        return Nvl_Certificacion;
+    }
+
+    public void setNvl_Certificacion(short nvl_Certificacion) {
+        Nvl_Certificacion = nvl_Certificacion;
+    }
+
+    public Arbitro(int id, String nombre, String apellido, String pais, short nvl_Certificacion) {
+        super(id, nombre, apellido, pais);
+        Nvl_Certificacion = nvl_Certificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Arbitro{" +
+                "Nvl_Certificacion=" + Nvl_Certificacion +
+                '}';
+    }
+}
--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,45 @@
 public class Persona {
+private int Id;
+private String Nombre;
+private String Apellido;
+private String Pais;
 
+    public int getId() {
+        return Id;
+    }
 
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public Persona(int id, String nombre, String apellido, String pais) {
+        Id = id;
+        Nombre = nombre;
+        Apellido = apellido;
+        Pais = pais;
+    }
 }
