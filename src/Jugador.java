fa09c92348f64c0260f9f2cf595bdbf288ee698d
AleSpinelli2 <alejospinelli2@gmail.com>
2023-10-18 12:04:21 -0300

    Clase Jugador y toString en Persona

---
+++ src/Jugador.java
@@ -0,0 +1,31 @@
+public class Jugador extends Persona{
+    private int elo;
+    private short edad;
+
+    public int getElo() {
+        return elo;
+    }
+
+    public void setElo(int elo) {
+        this.elo = elo;
+    }
+
+    public short getEdad() {
+        return edad;
+    }
+
+    public void setEdad(short edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +super.toString()+ "elo=" + elo + ", edad=" + edad + '}';
+    }
+
+    public Jugador(int id, String nombre, String apellido, String pais, int elo, short edad) {
+        super(id, nombre, apellido, pais);
+        this.elo = elo;
+        this.edad = edad;
+    }
+}
